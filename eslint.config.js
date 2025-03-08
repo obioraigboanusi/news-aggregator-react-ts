167c660949a0204cc3cc070fa2a8bc46a5ec2ca8
Obiora Igboanusi <66188726+obioraigboanusi@users.noreply.github.com>
2025-03-08 19:07:19 +0100

    chore: setup eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,9 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', "node_modules", ".pnp*"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, 'prettier'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended,  'plugin:prettier/recommended' ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
